775d50ec2e42dd63d35bcd573745bad766d51d62
Ashraf Hossain <ashrafhossain@Ashrafs-MacBook-Pro.local>
2019-07-23 13:07:18 +0600

    Updates

---
+++ precache-manifest.74b504d7afe1a5d32f06246d2d2bebe7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf669cb2dd39adf1f3bf4e6ec1ba896",
+    "url": "/comical_fourier/index.html"
+  },
+  {
+    "revision": "70013fd55640ceeec073",
+    "url": "/comical_fourier/static/css/main.810118c5.chunk.css"
+  },
+  {
+    "revision": "39668ba9ee0e7a91a6e2",
+    "url": "/comical_fourier/static/js/2.fce1b0e3.chunk.js"
+  },
+  {
+    "revision": "70013fd55640ceeec073",
+    "url": "/comical_fourier/static/js/main.2db7e7ae.chunk.js"
+  },
+  {
+    "revision": "54222729d816d8d44953",
+    "url": "/comical_fourier/static/js/runtime~main.db36209e.js"
+  }
+]);
