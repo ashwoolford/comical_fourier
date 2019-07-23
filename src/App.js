8604bd50cb96ffdb03de3bcbf68863ce1e34ae87
Ashraf Hossain <ashrafhossain@Ashrafs-MacBook-Pro.local>
2019-07-23 19:45:01 +0600

    Start with create-react-app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home"
 import About from "./components/About"
 import Projects from "./components/Projects"
 import FFTSimulator from './components/FFTSimulator'
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 export class App extends Component {
   
   render() {
