0f431b191dda6c4cf97202585deab34afc8629f1
Ashraf Hossain <ashrafhossain@Ashrafs-MacBook-Pro.local>
2019-07-26 22:16:51 +0600

    Updates

--- precache-manifest.55c824a9c06cc5ac7c504f49e5f94547.js
+++ precache-manifest.55c824a9c06cc5ac7c504f49e5f94547.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dea03eeb68a4e29c1f3108bf641620c7",
+    "revision": "ca4843e72d0d88340acc6b5f157abb75",
     "url": "/comical_fourier/index.html"
   },
   {
-    "revision": "bd7529eb1a50158b2da7",
-    "url": "/comical_fourier/static/css/main.5fa364fa.chunk.css"
+    "revision": "63b472527760ce300e86",
+    "url": "/comical_fourier/static/css/main.40ba71ad.chunk.css"
   },
   {
     "revision": "0ed4e49155ddcbcd1581",
     "url": "/comical_fourier/static/js/2.58f44494.chunk.js"
   },
   {
-    "revision": "bd7529eb1a50158b2da7",
-    "url": "/comical_fourier/static/js/main.fbfe48b0.chunk.js"
+    "revision": "63b472527760ce300e86",
+    "url": "/comical_fourier/static/js/main.2598ef01.chunk.js"
   },
   {
     "revision": "54222729d816d8d44953",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/comical_fourier/precache-manifest.be170f445a2e16913270776c9a4c343f.js"
+  "/comical_fourier/precache-manifest.55c824a9c06cc5ac7c504f49e5f94547.js"
 );
 
 self.addEventListener('message', (event) => {
