7d0f3df47fa343787d28846c9edbcb9bfd7b7cc7
Ashraf Hossain <ashrafhossain@Ashrafs-MacBook-Pro.local>
2019-07-26 20:29:39 +0600

    changed route files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import Home from "./components/Home"
-//import About from "./components/About"
+import About from "./components/About"
 import Projects from "./components/Projects"
 import FFTSimulator from './components/FFTSimulator'
 import { HashRouter, Route, Link} from "react-router-dom";
@@ -14,17 +14,14 @@ export class App extends Component {
       <HashRouter basename='/'>
       <div>
         <MyNavbar />
-        <Route exact path="/" component={home} />
+        <Route exact path="/" component={Home} />
         <Route path="/simulate" component={FFTSimulator}></Route>
-        <Route path="/about" component={about}/>
+        <Route path="/about" component={About}/>
         <Route path="/projects" component={Projects}></Route>
       </div>
     </HashRouter>
     );
   }
 }
-
-const home = () => <Home />
-const about = () => <div><h2>about</h2></div>
 
 export default App;
--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -6,21 +6,26 @@ import { Link } from "react-router-dom";
 export default class MyNavbar extends Component {
   render() {
     return (
+      <div className="homepagenavbar">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/">ComicalFourier</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto" />
 
-    <div className="homepagenavbar">
-           <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/">ComicalFourier</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-          </Nav>
-          <Nav>
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+            <Nav>
+              <Nav.Link>
+                <Link to="/" className="nav-bar-link">Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/projects" className="nav-bar-link">Projects</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/about" className="nav-bar-link">About</Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
